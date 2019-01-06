b7767c913721f23941f1874dce3f58cd0ab7a7cc
Steve Faulkner <southpolesteve@gmail.com>
2019-01-06 18:32:43 -0500

    Don't reassign prev

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 module.exports = function(strings, ...expressions) {
   return strings.reduce((prev, curr, index) => {
     let value = expressions[index]
-    if (index === strings.length - 1) return (prev += curr)
+    if (index === strings.length - 1) return prev + curr
     if (value === undefined || value === null) {
       throw Error(`Expression after "${curr}" was null or undefined`)
     }
-    return (prev += curr + value)
+    return prev + curr + value
   }, '')
 }
